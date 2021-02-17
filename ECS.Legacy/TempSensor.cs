8464efbac751e67e55f9a20421a1c19d1d969cda
yuhu0006 <65534979+yuhu0006@users.noreply.github.com>
2021-02-17 10:02:19 +0100

    Add files via upload

---
+++ ECS.Legacy/TempSensor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ECS.Legacy
+{
+    internal class TempSensor
+    {
+        private Random gen = new Random();
+
+        public int GetTemp()
+        {
+            return gen.Next(-5, 45);
+        }
+
+        public bool RunSelfTest()
+        {
+            //HEJ
+            return true;
+        }
+    }
+}
